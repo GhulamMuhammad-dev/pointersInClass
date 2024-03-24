4d60e61bff69ee43a440a51a408b60e0063d4064
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-03-24 22:08:41 +0500

    practicing

--- program.cpp
+++ program.cpp
@@ -35,15 +35,18 @@ class Animal{
 int main(){
     Animal a1;
     Animal a2;
-    Animal *a3_ptr=&a1;
-    a3_ptr=&a2;
+
+    Animal *const a3_ptr=&a1;
+    // a3_ptr=&a2;
     a3_ptr->setName("dog");
 
     // a2.getName();
-    a1.getName();
+ 
 
-
-
+    cout<<a3_ptr<<endl;
+    cout<<&a1<<endl;
+    cout<<&a2<<endl;
+   
 
 
     return 0;
